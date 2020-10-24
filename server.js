8b70c71e404d4e4948c89f44e893c881db186338
Dominik Jasek <jasekdominik@seznam.cz>
2020-10-24 15:15:15 +0200

    Revert "disable ssl (https → http)"
    This reverts commit d2f85bd8c30581356571c456b3ccc22581ab27f8.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const sslRedirect = require('heroku-ssl-redirect');
+const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// app.use(sslRedirect());
+app.use(sslRedirect());
 app.use(express.static(__dirname + '/dist'));
 app.get(/.*/, function(req, res) {
 	res.sendFile(__dirname + '/dist/index.html');
