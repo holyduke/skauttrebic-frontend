9a9442bbc2898199fbdbc58554ee198eeeb65b96
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-04 19:13:35 +0100

    Revert "fix cors?"
    This reverts commit cdf53299d32c0febfcb03bd8c2abd5e8b235b5ab.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
-app.options('*', cors());
 app.use(sslRedirect());
 app.use(express.static(__dirname + '/dist'));
 app.get(/.*/, function(req, res) {
