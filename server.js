cdf53299d32c0febfcb03bd8c2abd5e8b235b5ab
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-04 19:06:18 +0100

    fix cors?

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
+app.options('*', cors());
 app.use(sslRedirect());
 app.use(express.static(__dirname + '/dist'));
 app.get(/.*/, function(req, res) {
