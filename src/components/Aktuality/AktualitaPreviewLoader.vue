f98a09aa65f13504346735be06075ecc243ad359
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-13 00:21:53 +0100

    new skeleton loader for aktualita preview

--- src/components/Aktuality/AktualitaPreviewLoader.vue
+++ src/components/Aktuality/AktualitaPreviewLoader.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto pa-4" outlined>
-    <v-skeleton-loader class="mx-auto my-3" type="chip, card-heading, text@2, actions"></v-skeleton-loader>
+    <v-skeleton-loader class="mx-auto my-3" type="card"></v-skeleton-loader>
   </v-card>
 </template>
 
