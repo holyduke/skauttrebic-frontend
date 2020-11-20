d395046d4516dd1b61a059ca7ea06e36ad6e38fb
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-20 16:27:49 +0100

    zonerama iframe skauti

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,8 +1,12 @@
 <template>
-  <v-container grid-list-xs class="my-3">
-    <h1 class="mb-0" id="Aktuality">Fotografie</h1>
-    <v-divider class="mb-2"></v-divider>
-    <p class="mt-5">Prosíme o nešíření odkazů dál.</p>
+  <v-container grid-list-xs class="my-3 px-4" >
+    <div class="main-container">
+      <h1 class="mb-0" id="Aktuality">Fotografie</h1>
+      <v-divider class="mb-2"></v-divider>
+      
+      <iframe class="px-2" src="https://www.zonerama.com/Embed/Account/246290?color=gray-dark&autoplay=false&vertical=false" style="width: 100%; height: 330px;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    </div>
+    <!-- <p class="mt-5">Prosíme o nešíření odkazů dál.</p>
     <div class="mt-15 d-flex flex-wrap justify-space-around">
             <v-btn :color="colors['skauti']" rounded class="white--text my-2" href="https://www.zonerama.com/2oddilskautu" target="_blank">Skauti</v-btn>
             <v-btn :color="colors['skautky']" rounded class="white--text my-2" href="https://skautky2.rajce.idnes.cz/" target="_blank">Skautky</v-btn>
@@ -10,7 +14,7 @@
             <v-btn :color="colors['světlušky']" rounded class="white--text my-2" href="https://svetluskytrebic.rajce.idnes.cz/" target="_blank">Světlušky</v-btn>
             <v-btn :color="colors['roveři']" rounded class="white--text my-2" href="https://www.zonerama.com/RR2oddil/796874" target="_blank">Roveři</v-btn>
             <v-btn :color="colors['oldskauti']" rounded class="white--text my-2" href="https://www.zonerama.com/KatkaJanickova/837732" target="_blank">Oldskauti</v-btn>
-    </div>
+    </div> -->
     
 
   </v-container>
@@ -27,6 +31,17 @@ export default {
       colors: Constants.colors,
     }
   },
+
+  methods: {
+    iframeStyles() {
+        this.frame = this.$refs.iframeContent.contentWindow;
+
+        const style =
+            '.pull-menu {display: none;} ' +
+            'body {color:' + this.text + ';} '
+        this.frame.postMessage(style, '*');
+    }
+  }
 };
 </script>
 
@@ -35,5 +50,9 @@ h1 {
   font-family: "skautbold";
   margin: 20px 0 20px 0;
   font-size: 2.5rem;
+}
+
+.main-container >>> .pull-menu  {
+  display: none !important;
 }
 </style>
