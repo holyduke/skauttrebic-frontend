299b88773b33a0cde3b506d6f995ff812bd194ba
Dominik Jasek <jasekdominik@seznam.cz>
2020-11-22 20:13:02 +0100

    added class zonerama

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,12 +1,19 @@
 <template>
-  <v-container grid-list-xs class="my-3 px-4" >
+  <v-container grid-list-xs class="my-3 px-4">
     <div class="main-container">
       <h1 class="mb-0" id="Aktuality">Fotografie</h1>
       <v-divider class="mb-2"></v-divider>
-      
-      <iframe class="px-2" src="https://www.zonerama.com/Embed/Account/246290?color=gray-dark&autoplay=false&vertical=false" style="width: 100%; height: 330px;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-    </div>    
-
+      <div class="zonerama">
+        <iframe
+          class="px-2"
+          src="https://www.zonerama.com/Embed/Account/246290?color=gray-dark&autoplay=false&vertical=false"
+          style="width: 100%; height: 330px"
+          scrolling="no"
+          frameborder="0"
+          allowTransparency="true"
+        ></iframe>
+      </div>
+    </div>
   </v-container>
 </template>
 
@@ -14,24 +21,23 @@
 import Constants from "@/constants.js";
 
 export default {
-  name: 'Photos',
+  name: "Photos",
 
   data() {
     return {
       colors: Constants.colors,
-    }
+    };
   },
 
   methods: {
     iframeStyles() {
-        this.frame = this.$refs.iframeContent.contentWindow;
+      this.frame = this.$refs.iframeContent.contentWindow;
 
-        const style =
-            '.pull-menu {display: none;} ' +
-            'body {color:' + this.text + ';} '
-        this.frame.postMessage(style, '*');
-    }
-  }
+      const style =
+        ".pull-menu {display: none;} " + "body {color:" + this.text + ";} ";
+      this.frame.postMessage(style, "*");
+    },
+  },
 };
 </script>
 
@@ -42,8 +48,11 @@ h1 {
   font-size: 2.5rem;
 }
 
+.zonerama {
+  margin-top: 5px;
+}
 
-.main-container >>> .pull-menu  {
+.main-container >>> .pull-menu {
   display: none !important;
 }
 </style>
